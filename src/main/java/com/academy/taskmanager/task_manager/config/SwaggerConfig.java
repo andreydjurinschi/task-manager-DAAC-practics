9d93c10ac4dca7b94d3465f01476f8154a3c13c1
Andrei <djurinschi.andreiai@gmail.com>
2025-06-19 00:52:36 +0300

    auth working

--- src/main/java/com/academy/taskmanager/task_manager/config/SwaggerConfig.java
+++ src/main/java/com/academy/taskmanager/task_manager/config/SwaggerConfig.java
@@ -9,48 +9,27 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 
 @Configuration
-@io.swagger.v3.oas.annotations.security.SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
-)
 public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Task Manager API")
-                        .description("API документация для Task Manager")
-                        .version("1.0"))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
-                                .name("bearerAuth")
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")));
-    }
-
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes("oauth2Scheme", new SecurityScheme()
+                .info(new Info().title("Task Manager API").version("v1"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("oauth2", new SecurityScheme()
                                 .type(SecurityScheme.Type.OAUTH2)
                                 .flows(new OAuthFlows()
                                         .authorizationCode(new OAuthFlow()
                                                 .authorizationUrl("http://localhost:8081/realms/task-manager/protocol/openid-connect/auth")
                                                 .tokenUrl("http://localhost:8081/realms/task-manager/protocol/openid-connect/token")
-                                                .scopes(new Scopes().addString("openid", "OpenID Connect scope"))
-                                        )
-                                )
-                        )
-                )
-                .addSecurityItem(new SecurityRequirement().addList("oauth2Scheme"));
+                                                .scopes(new Scopes()
+                                                        .addString("openid", "Access ID token")
+                                                        .addString("profile", "User profile")
+                                                        .addString("email", "User email"))))))
+                .addSecurityItem(new SecurityRequirement().addList("oauth2", List.of("openid")));
     }
 
 }
